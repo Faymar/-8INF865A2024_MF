00839021852af006fdd9435466c8e9ce606de9de
Mamadou Faye <mamadoufaye@DESKTOP-JQ5H90V.localdomain>
2024-09-05 02:15:43 -0400

    1er visualisation de l'image

--- app/src/main/java/com/faya/happybirthday/MainActivity.kt
+++ app/src/main/java/com/faya/happybirthday/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -15,6 +16,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -68,6 +70,15 @@ fun GreetingText(message: String,from: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     HappyBirthdayTheme {
-        GreetingText(message = "Happy birthday Faye!", from = "From Mouhamadou")
+        GreetingImage(message = "Happy birthday Faye!", from = "From Mouhamadou")
     }
+}
+
+@Composable
+fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier) {
+    val image = painterResource(R.drawable.androidparty)
+    Image(
+        painter = image,
+        contentDescription = null
+    )
 }
