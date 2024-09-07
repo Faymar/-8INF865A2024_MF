635e153d4107fc4ad468ada046beccb63afa0563
Mamadou Faye <mamadoufaye@DESKTOP-JQ5H90V.localdomain>
2024-09-07 00:34:16 -0400

    bonnes pratiques avec les textes

--- app/src/main/java/com/faya/happybirthday/MainActivity.kt
+++ app/src/main/java/com/faya/happybirthday/MainActivity.kt
@@ -21,6 +21,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -38,8 +39,8 @@ class MainActivity : ComponentActivity() {
                    color = MaterialTheme.colorScheme.background
                ){
                     GreetingImage(
-                        message = "Happy birthday Faye!",
-                        from = "From Mouhamadou",
+                        message = stringResource(R.string.happy_birthday_text),
+                        from = stringResource(R.string.signature_text),
                     )
                }
             }
@@ -73,7 +74,10 @@ fun GreetingText(message: String,from: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     HappyBirthdayTheme {
-        GreetingImage(message = "Happy birthday Faye!", from = "From Mouhamadou")
+        GreetingImage(
+            message = stringResource(R.string.happy_birthday_text),
+            from = stringResource(R.string.signature_text)
+        )
     }
 }
 
@@ -88,8 +92,8 @@ fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier) 
             alpha = 0.5F
         )
         GreetingText(message = message, from = from, modifier = Modifier
-                .fillMaxSize()
-                .padding(8.dp))
+            .fillMaxSize()
+            .padding(8.dp))
     }
 
 }
