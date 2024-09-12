32e114e77c986e64c757026793fc394e1bba6bcd
Mamadou Faye <mamadoufaye@DESKTOP-JQ5H90V.localdomain>
2024-09-11 23:34:57 -0400

    step10

--- Calculatrice/app/src/main/java/com/faya/calculatrice/MainActivity.kt
+++ Calculatrice/app/src/main/java/com/faya/calculatrice/MainActivity.kt
@@ -64,6 +64,9 @@ fun TipTimeLayoutPreview() {
 
 @Composable
 fun TipTimeLayout() {
+    var amountInput by remember { mutableStateOf("") }
+    val amount = amountInput.toDoubleOrNull() ?: 0.0
+    val tip = CalculateTip(amount)
     Column(
         modifier = Modifier
             .statusBarsPadding()
@@ -79,9 +82,13 @@ fun TipTimeLayout() {
                 .padding(bottom = 16.dp, top = 40.dp)
                 .align(Alignment.Start)
         )
-        EditNumberField(modifier = Modifier.padding(bottom = 30.dp).fillMaxWidth())
+        EditNumberField(
+            value = amountInput,
+            onValueChange = { amountInput = it },
+            modifier = Modifier.padding(bottom = 30.dp).fillMaxWidth()
+        )
         Text(
-            text = stringResource(R.string.tip_amount, "$0.00"),
+            text = stringResource(R.string.tip_amount, tip.toString()),
             style = MaterialTheme.typography.displaySmall
         )
         Spacer(modifier = Modifier.height(150.dp))
@@ -95,14 +102,17 @@ private fun CalculateTip(amount: Double, tipPercent: Double = 15.0): String? {
 
 //var amountInput: MutableState<String> = mutableStateOf("0")
 @Composable
-fun EditNumberField(modifier: Modifier = Modifier) {
-    var amountInput by remember { mutableStateOf("")}
+fun EditNumberField(
+    value: String,
+    onValueChange: (String) -> Unit,
+    modifier: Modifier = Modifier
+) {
     TextField(
-        value = amountInput,
-        onValueChange = { amountInput = it },
+        value = value,
+        onValueChange = onValueChange,
         singleLine = true,
         label = { Text(stringResource(R.string.bill_amount)) },
         keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
-        modifier = Modifier
+        modifier = modifier
     )
 }
